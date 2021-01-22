25c7c5c3c1cf619929c66c65b090d4749b47b66b
jltkd <jl@turnkeydigital.com>
2021-01-22 11:14:28 -0600

    Added logo to index.js

--- pages/index.js
+++ pages/index.js
@@ -11,46 +11,20 @@ import createMarkup from '../functions/createMarkup'
 function IndexPage() {
   return (
     <section className="root-container">
-      <section className="responsive-container homepage-container">
-
+      <section className="homepage-container">
+        <img className="dim logo" src="https://www.deltaadsorbents.com/wp-content/uploads/2021/01/email-logo.png" />
         <style jsx>{`
         .homepage-container {
           display: flex;
-          justify-content: space-between;
-          flex-wrap: wrap;
-          padding-top:2em;
+          justify-content: center;
+          align-items: center;
+          width: 100vw;
+          height: 100vh;
         }
-        tr {
-          display:flex;
-        }
-        td {
-          display:block;
-          padding:1em;
-        }
-        tr td:nth-of-type(1){
-          width:65%;
-        }
-        tr td:nth-of-type(2){
-          width:15%;
-          display:flex;
-          align-items:center;
-        }
-        tr td:nth-last-of-type(1){
-          width:20%;
-        }
-        .productImage {
-          width:100%;
-        }
-        ul {
-          list-style:disc;
-          margin-left:10px;
-        }
-        ul a {
-          cursor:pointer;
-          transition:500ms;
-        }
-        ul a:hover {
-          opacity:.5;
+        img {
+          width: 100%;
+          max-width: 500px;
+          height: auto;
         }
       `}</style>
       </section>
