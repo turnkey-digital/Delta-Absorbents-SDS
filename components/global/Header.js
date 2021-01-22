abf3ac57820dafb0828ff3e44f592bc89fd19e8e
jltkd <jl@turnkeydigital.com>
2021-01-22 10:40:29 -0600

    Update Header.js

--- components/global/Header.js
+++ components/global/Header.js
@@ -20,11 +20,11 @@ function Header({ seo, title, json_ld }) {
                     </script>
                     <link rel="stylesheet" href="https://use.typekit.net/aiw3hgg.css" />
                     {/* <link rel="shortcut icon" href={favicon} /> */}
-                    <link rel="shortcut icon" href="https://delta.server9.turnkeydigital.dev/wp-content/uploads/2020/10/favicon.ico" />
+                    <link rel="shortcut icon" href="https://deltaadsorbents.com/wp-content/uploads/2020/10/favicon.ico" />
                 </Head>
                 <div className="container left">
                     <Link href="/">
-                        <img className="dim logo" src="https://delta.server9.turnkeydigital.dev/wp-content/uploads/2020/09/unnamed-file.png" />
+                        <img className="dim logo" src="https://deltaadsorbents.com/wp-content/uploads/2020/09/unnamed-file.png" />
                     </Link>
                 </div>
                 <div className="container right header_copy">
