8599b7a8a34a380a0bbc87a81cabdfd364bbd701
Madison <66797979+Mcook9887@users.noreply.github.com>
2024-11-27 13:17:30 -0600

    Update Header.js

--- components/global/Header.js
+++ components/global/Header.js
@@ -1,6 +1,7 @@
 // Utilities
 import Head from 'next/head'
 import Link from 'next/link'
+import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
 
 // Dependancies
 var borderStyles = '1px solid black'
